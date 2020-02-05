c54cf38635507bd23d25330da8eab14e16f58ec7
Arthur Siqueira <arthur.siq5@gmail.com>
2020-02-05 16:49:23 -0300

    adicionados itens iniciais

--- src/app/reducers/tutorial.reducer.ts
+++ src/app/reducers/tutorial.reducer.ts
@@ -2,13 +2,59 @@ import { Action } from '@ngrx/store';
 import { Tutorial } from '../models/tutorial.model';
 import * as TutorialActions from '../actions/tutorial.actions';
 
-const initialState: Tutorial = {
-  name: 'Initial Tutorial',
-  url: 'google.com'
-}
+const initialState: Tutorial[] = [
+  {
+    name: 'Initial Tutorial',
+    url: 'google.com'
+  },
+  {
+    name: 'Initial Tutorial1',
+    url: 'google.com'
+  },
+  {
+    name: 'Initial Tutorial2',
+    url: 'google.com'
+  },
+  {
+    name: 'Initial Tutorial3',
+    url: 'google.com'
+  },
+  {
+    name: 'Initial Tutorial4',
+    url: 'google.com'
+  },
+  {
+    name: 'Initial Tutorial5',
+    url: 'google.com'
+  },
+  {
+    name: 'Initial Tutorial6',
+    url: 'google.com'
+  },
+  {
+    name: 'Initial Tutorial7',
+    url: 'google.com'
+  },
+  {
+    name: 'Initial Tutorial8',
+    url: 'google.com'
+  },
+  {
+    name: 'Initial Tutorial9',
+    url: 'google.com'
+  },
+  {
+    name: 'Initial Tutorial10',
+    url: 'google.com'
+  },
+  {
+    name: 'Initial Tutorial11',
+    url: 'google.com'
+  },
+];
 
 export function reducer(
-  state: Tutorial[] = [initialState],
+  state: Tutorial[] = initialState,
   action: TutorialActions.Actions
 ){
   switch(action.type){
